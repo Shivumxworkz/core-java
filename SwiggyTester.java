d605cc6a1a1611c09da6c1521f2c8dfc66da3838
Shivumxworkz <111728158+Shivumxworkz@users.noreply.github.com>
2022-09-23 01:31:28 +0530

    Delete SwiggyTester.java

--- SwiggyTester.java
+++
@@ -1,9 +0,0 @@
-class SwiggyTester{
-	public static void main(String swiggy[]){
-		/*String ref=Swiggy.takeOrder("pizza");
-		System.out.println("Finally, you are order is Ready..."+ref);*/
-		System.out.println(Swiggy.takeOrder("pizza"));
-		System.out.println(Swiggy.takeOrder("Pastries",7));
-		System.out.println("Finally, you are order is Ready...");
-	}
-}
